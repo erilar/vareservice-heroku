a1083f7cd942c0bbb963753d9e9eae248aec58c6
erilar <Simon.Royrvik.Andresen@iteraconsulting.no>
2011-09-12 15:54:19 +0200

    Added WS method for adding a vare

--- src/main/java/no/andsim/service/VareService.java
+++ src/main/java/no/andsim/service/VareService.java
@@ -1,9 +1,12 @@
 package no.andsim.service;
 
+import generated.Vare;
+
 import javax.jws.WebService;
 
 @WebService
 public interface VareService {
     String sayHi(String text);
+    boolean sendVare(Vare vare);
 }
 
--- src/main/java/no/andsim/service/VareServiceImpl.java
+++ src/main/java/no/andsim/service/VareServiceImpl.java
@@ -1,13 +1,28 @@
 
 package no.andsim.service;
 
+import generated.Vare;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "no.andsim.service.VareService")
 public class VareServiceImpl implements VareService {
 
+	private Map<Long,Vare> varer = new HashMap<Long, Vare>(); 
+	
     public String sayHi(String text) {
         return "Hello from the VareService Mr " + text;
+    }
+    
+    public boolean sendVare(Vare vare){
+    	if(!varer.containsKey(vare.getBarcode())){
+    		varer.put(vare.getBarcode(), vare);
+    		return true;
+    	}
+    	return false;
     }
 }
 
